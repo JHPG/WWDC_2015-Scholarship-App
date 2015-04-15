21435e4d9d197a64692e541fe9030b31f5fbbc27
Jorge H. P. Garcia <j.hrique@hotmail.com>
2015-04-15 18:17:19 -0300

    Initial Commit

---
+++ Jorge Garcia/ViewController.swift
@@ -0,0 +1,32 @@
+//
+//  ViewController.swift
+//  Jorge Garcia
+//
+//  Created by Jorge Henrique P. Garcia on 4/15/15.
+//  Copyright (c) 2015 Jorge Henrique P. Garcia. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var imgFoto: UIImageView!
+    @IBOutlet weak var img2: UIImageView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    
+    override func viewDidAppear(animated: Bool) {
+   
+    }
+    
+}
+
